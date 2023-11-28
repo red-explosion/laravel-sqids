ee2c71ea395ffe219f4730118ae16f424f944dfa
Ben Sherred <ben@sherred.co.uk>
2023-11-28 20:09:10 +0000

    test: route model binding without a prefix

--- tests/RouteModelBindingTest.php
+++ tests/RouteModelBindingTest.php
@@ -12,6 +12,16 @@ it('can bind a model from a sqid', function (): void {
         ->assertContent(value: $customer->name);
 });
 
+it('can bind a model from a sqid without a prefix', function (): void {
+    config()->set('sqids.prefix_class');
+
+    $customer = CustomerFactory::new()->create();
+
+    $this
+        ->get(uri: "/customers/{$customer->sqid}")
+        ->assertContent(value: $customer->name);
+});
+
 it('returns a 404 if the sqid is invalid', function (): void {
     $this
         ->get(uri: "/customers/invalid-sqid")
