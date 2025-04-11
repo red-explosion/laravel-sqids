259707c202d56f0008d43906ecd7714042114294
Ben Carr <ben.carr@me.com>
2025-04-11 06:10:00 -0400

    fix: scoped route model binding (#13)
    Co-authored-by: Ben Sherred <ben@sherred.co.uk>

--- workbench/app/Models/Charge.php
+++ workbench/app/Models/Charge.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Workbench\App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use RedExplosion\Sqids\Concerns\HasSqids;
 
 class Charge extends Model
@@ -12,4 +13,12 @@ class Charge extends Model
     use HasSqids;
 
     protected string $sqidPrefix = 'ch';
+
+    /**
+     * @return BelongsTo<Customer,$this>
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
